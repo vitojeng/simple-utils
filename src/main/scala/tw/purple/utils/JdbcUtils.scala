a64172e31b5b71612fc99f6f68f88f371c50087c
vitojeng <vito@purple.tw>
2022-06-20 12:17:07 +0800

    JdbcUtils - Use scala.util.Using to manage datasource and connection

--- src/main/scala/tw/purple/utils/JdbcUtils.scala
+++ src/main/scala/tw/purple/utils/JdbcUtils.scala
@@ -3,6 +3,7 @@ package tw.purple.utils
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 
 import java.sql.{Connection, DriverManager, PreparedStatement, ResultSet}
+import javax.sql.DataSource
 import scala.util.Using
 
 
@@ -30,13 +31,20 @@ object JdbcUtils {
       if (queryString.nonEmpty) url + "?" + queryString else url
     }
 
-    def connection(url: String): Connection =
+    def getConnection(url: String): Connection =
       DriverManager.getConnection(url)
 
-    def connection(url: String, username: String, password: String): Connection =
-      DriverManager.getConnection(url, username, password)
+    def connection[T](url: String)(f: Connection => T): T = {
+      val conn = DriverManager.getConnection(url)
+      Using.resource(conn) { c => f(c) }
+    }
 
-    def datasource(url: String, username: String, password: String): HikariDataSource = {
+    def connection[T](url: String, username: String, password: String)(f: Connection => T): T = {
+      val conn = DriverManager.getConnection(url, username, password)
+      Using.resource(conn) { c => f(c) }
+    }
+
+    def getDatasource(url: String, username: String, password: String): HikariDataSource = {
       val hikariConfig = new HikariConfig
       hikariConfig.setJdbcUrl(url)
       hikariConfig.setUsername(username)
@@ -47,9 +55,7 @@ object JdbcUtils {
   }
 
 
-
-  implicit class ConnectionOps(private val conn: Connection) extends AnyVal {
-
+  object ConnectionImports {
     private def _iteratorOf[T](resultSet: ResultSet)(f: ResultSet => T): Iterator[T] = {
       new Iterator[T] {
         def hasNext: Boolean = resultSet.next()
@@ -75,7 +81,14 @@ object JdbcUtils {
       }
     }
 
-    def query[T](sql: String, parameters: Seq[Any] = Seq.empty)(f: ResultSet => T): Seq[T] = {
+    def update(sql: String, parameters: Seq[Any] = Seq.empty)(implicit conn: Connection): Int = {
+      Using.resource(conn.prepareStatement(sql)) { statement =>
+        _setParameters(statement, parameters)
+        statement.executeUpdate()
+      }
+    }
+
+    def query[T](sql: String, parameters: Seq[Any] = Seq.empty)(f: ResultSet => T)(implicit conn: Connection): Seq[T] = {
       Using.Manager { use =>
         val statement = use(conn.prepareStatement(sql))
         _setParameters(statement, parameters)
@@ -84,13 +97,65 @@ object JdbcUtils {
       }.get
     }
 
-    def update(sql: String, parameters: Seq[Any] = Seq.empty): Int = {
-      Using.resource(conn.prepareStatement(sql)) { statement =>
-        _setParameters(statement, parameters)
-        statement.executeUpdate()
-      }
-    }
-
   }
 
+  implicit class DataSourceOps(private val ds: DataSource) extends AnyVal {
+    def connection[T](f: Connection => T): T = {
+      Using.resource(ds.getConnection) { c =>
+        f(c)
+      }
+    }
+  }
+
+
+//  implicit class ConnectionOps(private val conn: Connection) extends AnyVal {
+//
+//    private def _iteratorOf[T](resultSet: ResultSet)(f: ResultSet => T): Iterator[T] = {
+//      new Iterator[T] {
+//        def hasNext: Boolean = resultSet.next()
+//
+//        def next(): T = f(resultSet)
+//      }
+//    }
+//
+//    private def _setParameters(statement: PreparedStatement, parameters: Seq[Any]): Unit = {
+//      statement.clearParameters()
+//      if (parameters.nonEmpty) {
+//        parameters.zipWithIndex.foreach { case (value, i) =>
+//          val parameterIndex = i + 1
+//          value match {
+//            case v: String => statement.setString(parameterIndex, v)
+//            case v: Long => statement.setLong(parameterIndex, v)
+//            case v: Int => statement.setInt(parameterIndex, v)
+//            case v: Boolean => statement.setBoolean(parameterIndex, v)
+//            case null => statement.setObject(parameterIndex, null)
+//            case v => throw new RuntimeException("The query parameter type not supported: " + v.getClass.toString)
+//          }
+//        }
+//      }
+//    }
+//
+//    def apply(f: Connection => Unit): Unit = {
+//      f(conn)
+//    }
+//
+//    def query[T](sql: String, parameters: Seq[Any] = Seq.empty)(f: ResultSet => T): Seq[T] = {
+//      Using.Manager { use =>
+//        val statement = use(conn.prepareStatement(sql))
+//        _setParameters(statement, parameters)
+//        val rs = use(statement.executeQuery())
+//        _iteratorOf(rs)(f).toSeq
+//      }.get
+//    }
+//
+//    def update(sql: String, parameters: Seq[Any] = Seq.empty): Int = {
+//      Using.resource(conn.prepareStatement(sql)) { statement =>
+//        _setParameters(statement, parameters)
+//        statement.executeUpdate()
+//      }
+//    }
+//
+//  }
+
 }
+
--- src/test/scala/tw/purple/utils/JdbcUtilSuite.scala
+++ src/test/scala/tw/purple/utils/JdbcUtilSuite.scala
@@ -1,7 +1,9 @@
 package tw.purple.utils
 
+import com.zaxxer.hikari.HikariDataSource
 import org.testcontainers.containers.PostgreSQLContainer
 import tw.purple.utils.JdbcUtils._
+import tw.purple.utils.JdbcUtils.ConnectionImports._
 
 import scala.util.Using
 
@@ -11,33 +13,36 @@ class JdbcUtilSuite extends munit.FunSuite {
   val container = PostgresUtils.newContainer("postgres", "123", dbName, "sql/init_postgresql.sql")
   var ip: String = ""
   var port: Int = 0
+  var dataSource: HikariDataSource = _
+  var jdbc: JdbcConnect = _
+  var url: String = _
 
   override def beforeAll(): Unit = {
     container.start()
     ip = container.getHost
     port = container.getMappedPort(PostgreSQLContainer.POSTGRESQL_PORT)
-  }
-
-  override def afterAll(): Unit = {
-    container.stop()
-  }
-
-  var jdbc: JdbcConnect = _
-  var url: String = _
-
-  override def beforeEach(context: BeforeEach): Unit = {
     jdbc = JdbcConnect(POSTGRES, ip, port, dbName)
     url = jdbc.url()
     println(url)
+    dataSource = jdbc.getDatasource(url, "postgres", "123")
+  }
+
+  override def afterAll(): Unit = {
+    dataSource.close()
+    container.stop()
+  }
+
+
+  override def beforeEach(context: BeforeEach): Unit = {
   }
 
   override def afterEach(context: AfterEach): Unit = {
   }
 
-  test("connection - query") {
+  test("connection from DriverManager") {
     val sql = "SELECT name FROM passengers"
-    Using.resource(jdbc.connection(url, "postgres", "123")) { conn =>
-      val lines: Seq[String] = conn.query(sql) { rs =>
+    jdbc.connection(url, "postgres", "123") { implicit conn =>
+      val lines: Seq[String] = query(sql) { rs =>
         rs.getString(1)
       }
       assertEquals(lines, Seq("Jack", "Anna", "Wonder", "Stacy", "Stevie", "Harry"))
@@ -46,23 +51,19 @@ class JdbcUtilSuite extends munit.FunSuite {
 
   test("query with parameter") {
     val sql = "SELECT name FROM passengers where id>=?"
-    Using.Manager { use =>
-      val ds = use(jdbc.datasource(url, "postgres", "123"))
-      val conn = use(ds.getConnection)
-      val lines: Seq[String] = conn.query(sql, Seq(3)) { rs =>
+    dataSource.connection { implicit conn =>
+      val lines: Seq[String] = query(sql, Seq(3)) { rs =>
         rs.getString(1)
       }
       assertEquals(lines, Seq("Wonder", "Stacy", "Stevie", "Harry"))
     }
   }
 
-  test("connection - update") {
-    Using.Manager { use =>
-      val ds = use(jdbc.datasource(url, "postgres", "123"))
-      val conn = use(ds.getConnection)
-      val count = conn.update("update passengers set name='NewName' where id>=?", Seq(5))
-      assertEquals(count, 2)
+  test("update with parameters") {
+    val count = dataSource.connection { implicit conn =>
+      update("update passengers set name='NewName' where id>=?", Seq(5))
     }
+    assertEquals(count, 2)
   }
 
 
